cee94b3a0e28070c3298ee106b34775c9dd3adf8
Faruoq01 <faruk.aminu@mabevic.com>
2021-11-29 07:23:30 -0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,16 @@ import './Footer.scss';
 const Footer = () => {
     return(
         <div id='main-container'>
-            <div id="inner-header"></div>
+            <div id="inner-header">
+                <div id='name'>
+                    <div id='save'>
+                        <div><h2>SAVE & INVEST WHILE <br/> YOU SPEND & PURCHASE </h2></div>
+                    </div>
+                </div>
+                <div id='store'>
+
+                </div>
+            </div>
         </div>
     )
 }
