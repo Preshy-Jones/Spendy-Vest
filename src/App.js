7872071e7e72d7eda4e7e19ef59df7bcec66f22d
Faruoq01 <faruk.aminu@mabevic.com>
2021-12-03 21:47:44 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.scss';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './screens/Home';
+import About from './screens/About';
+import Contact from './screens/Contact';
+import FAQ from './screens/FAQ';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <div id='main-body'></div>
+      <About />
       <Footer />
     </div>
   );
