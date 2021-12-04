61e031dabd601a7734678e621f26aa781398495c
Faruoq01 <faruk.aminu@mabevic.com>
2021-12-04 02:49:53 -0800

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <About />
+      <FAQ />
       <Footer />
     </div>
   );
--- src/screens/FAQ.js
+++ src/screens/FAQ.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './FAQ.scss';
+import pic4 from '../assets/pic4.png';
 
 const FAQ = () => {
     return(
-        <div>
-            <h1>About</h1>
+        <div id= "home-container3">
+            <div id="main-about-container2">
+                <div id="about-text2">
+                    FREQUENTLY ASKED <br /> QUESTIONS
+                    <div id="about-text3">
+                        Some things you may want to know, if you can't find it, call us.
+                    </div>
+                </div>
+                
+                <div id="about-image2">
+                    <img src={pic4} alt="icon" />
+                </div>
+            </div>
         </div>
     )
 }
