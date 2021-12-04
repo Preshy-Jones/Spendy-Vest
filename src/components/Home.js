250d4aa89704daedb550d6805bba6513f780b6cb
peterchijioke <peterchijioke1@gmail.com>
2021-12-04 10:30:46 +0100

    spendi

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import "./Home.scss";
+
+const Body = () => (
+  <div className="main">
+    <p className="topLefttext1">
+      SAVE IN BITS <br />
+      EVERY
+      {/* <h1 style={{ backgroundColor: "#F2C438" }}> */}
+      TIME YOU SPEND.
+      <br /> EARN DOLLARS DAILY.
+    </p>
+    <div className="topRightDiv"></div>
+  </div>
+);
 
 function Home() {
   return (
     <div>
-      <Header />
-      <Footer />
+      {/* <Header /> */}
+      <Body />
+      {/* <Footer /> */}
     </div>
   );
 }
