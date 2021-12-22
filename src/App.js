8abe903a6d8cf024147a49d23b853734dfed38c3
Precious Adedibu <adedibuprecious@gmail.com>
2021-12-23 00:43:54 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.scss';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './screens/Home';
-import About from './screens/About';
-import Contact from './screens/Contact';
-import FAQ from './screens/FAQ';
-
+import "./App.scss";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./screens/Home";
+import About from "./screens/About";
+import Contact from "./screens/Contact";
+import FAQ from "./screens/FAQ";
 
 function App() {
   return (
